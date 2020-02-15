f4207139bc051a9b36ceec09b319e39f6053ebfd
Nikos <nikoikonomou92@gmail.com>
2020-02-15 17:05:36 +0200

    added hello web server

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => res.send('Hello Balena!'));
+app.listen(port, () => console.log(`Example app listening on port=${port}!`));
+
